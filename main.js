b0f2b1bab6430bdf0b3dfe2f57ada267f1931241
Mangayarkarasi Jeyachandran <mangayarkarasi.j@GAVSTECH.COM>
2020-09-09 00:20:24 +0530

    displayed data

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 
+var rowsCount;
+(function intialLoad()
+{
+    rowsCount = window.prompt('Enter the number of rows to be displayed per page?');
+})();
+
+noOfPagination = Math.floor(100/rowsCount);
+console.log(noOfPagination);
+
+// get JSON data;
+var myJSONData;
+
+function getJSONData(filepath, callback)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", filepath, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(this.readyState === 4 && this.status === 200)
+        {
+            callback(this.response);
+        }
+    }
+
+    rawFile.send();
+}
+
 var outerDiv = document.createElement('div');
 outerDiv.style.height = '100px';
 outerDiv.style.lineHeight = '100px';
@@ -11,22 +39,18 @@ outerDiv.style.borderRadius = '4px';
 
 document.body.appendChild(outerDiv);
 
-var first = CreatePaginationElement('First');
-var previous = CreatePaginationElement('Previous');
-var num1 = CreatePaginationElement('1');
-var num2 = CreatePaginationElement('2');
-var num3 = CreatePaginationElement('3');
-var num4 = CreatePaginationElement('4');
-var num5 = CreatePaginationElement('5');
-var num6 = CreatePaginationElement('6');
-var num7 = CreatePaginationElement('7');
-var num8 = CreatePaginationElement('8');
-var num9 = CreatePaginationElement('9');
-var num10 = CreatePaginationElement('10');
-var next = CreatePaginationElement('Next');
-var last = CreatePaginationElement('Last');
+var dataDiv = document.createElement('div');
+dataDiv.style.minHeight = '300px';
+dataDiv.style.marginTop = '30px';
+dataDiv.style.marginLeft = 'auto';
+dataDiv.style.marginRight = 'auto';
+dataDiv.style.padding = '20px';
+dataDiv.style.boxSizing = 'border-box';
+dataDiv.style.backgroundColor = '#fafafa';
+dataDiv.style.borderRadius = '4px';
+dataDiv.style.boxShadow = '1px 2px 3px 1px #c7c7c7';
 
-outerDiv.append(first, previous, num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, next, last);
+document.body.appendChild(dataDiv);
 
 function CreatePaginationElement(text)
 {
@@ -45,7 +69,56 @@ function CreatePaginationElement(text)
     {
         this.style.backgroundColor = '#337abc';
         this.style.color ='white';
+        SetOthersInactive(element);
     })
 
     return element;
 }
+
+function SetOthersInactive(element)
+{
+    let childs = outerDiv.querySelectorAll('div');
+    for(let child of childs)
+    {
+        if(child.innerText != element.innerText)
+        {
+            child.style.backgroundColor ='white';
+            child.style.color ='#8566d6';
+        }
+    }
+
+    getData(element);
+}
+
+function getData(element)
+{
+    let end = element.innerText * rowsCount;
+    let start = end - rowsCount + 1;
+    console.log("start", start);
+    console.log("end", end);
+    let filteredArray = myJSONData.slice(start, end);
+    console.log('filtered array', filteredArray);
+    dataDiv.innerText = JSON.stringify(filteredArray);
+}
+
+getJSONData('https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json',function(data)
+{
+    myJSONData = JSON.parse(data);
+    console.log(myJSONData);
+    var first = CreatePaginationElement('First');
+    var previous = CreatePaginationElement('Previous');
+    outerDiv.append(first, previous);
+
+    var variable = 'num';
+    for(let i = 1; i <= noOfPagination; i++)
+    {
+        window[variable+i] = CreatePaginationElement(i);
+        outerDiv.append(window[variable+i]) 
+    }
+
+    var next = CreatePaginationElement('Next');
+    var last = CreatePaginationElement('Last');
+    outerDiv.append(next, last);
+});
+ 
+
